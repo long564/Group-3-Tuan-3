af0b35b68785b2a016decb789a0acf838802f12e
tranhien1908 <tranhien190805@gmail.com>
2024-09-27 01:19:24 +0700

    baitapcuahien

--- bai1.java
+++ bai1.java
@@ -4,14 +4,12 @@ public static void main(String[] args) {
     Scanner sc= new Scanner(System.in);
     System.out.println("Nhap mot so nguyen: ");
     int x= sc.nextInt();
-    if (x>0) {
+    if (x>0) 
         System.out.println("so "+x+" la so nguyen duong");
-    }
     else if (x<0) {
         System.out.println("so "+x+" la so nguyen am");
     }
     else
         System.out.println("So vua nhap la so 0");
-    System.out.println("xinchao");
 
 }
